c0d385902a14ed68ce472a9c073a34c5581b1984
blacklytning <blacklytning@yandex.com>
2025-07-22 14:11:52 +0530

    Continue executing code even if invalid token instead of mandatory 401

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ class Token(BaseModel):
     token_type: str = "bearer"
 
 # OAuth2 scheme
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login",auto_error=False)
 
 # Dependency to get current user from JWT
 def get_current_user(token: str = Depends(oauth2_scheme)):
